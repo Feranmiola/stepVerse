decf3773fdd264ffc8a8ac0cd8b2144544899024
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-30 16:11:27 +0100

    "Refactored Movement component: updated layout, styling, and added 'Join Community' button"

--- src/components/Movement.tsx
+++ src/components/Movement.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Movement = () => {
   return (
-    <div className='w-full pt-20 flex items-center justify-center space-y-5'>
-      <div className='w-[1200px] flex items-center justify-center flex-col space-y-10'>
-
-        <div className='w-[481px] h-[109px] flex flex-col justify-between items-center'>
-          <p className='text-[#191918] text-[32px] font-medium'>Be Part of the Movement</p>
-          <p className='text-center w-full text-[#4C4C4C] text-base'>Join a growing community of users committed to better health. <br/> Together, we can reduce sedentary behavior and build <br/> healthier, happier lives for millions around the globe.</p>
+    <div className="w-full pt-20 flex flex-col items-center justify-center space-y-5">
+      <div className="flex flex-col space-y-5 items-center justify-center">
+        <div className="w-[481px] h-[109px] flex flex-col justify-between items-center">
+          <p className="text-[#191918] text-[32px] font-medium">
+            Be Part of the Movement
+          </p>
+          <p className="text-center w-full text-[#4C4C4C] text-base">
+            Join a growing community of users committed to better health. <br />{" "}
+            Together, we can reduce sedentary behavior and build <br />{" "}
+            healthier, happier lives for millions around the globe.
+          </p>{" "}
         </div>
 
-        <div className='w-full h-[443px] rounded-2xl bg-neutral-600'>
-
+        <div className="w-[172px] h-[46px] rounded-[45px] flex items-center justify-center cursor-pointer transition ease-in-out  text-white font-medium text-base hover:text-[#191918] bg-[#191918] hover:bg-transparent border-transparent border-[1px] hover:border-[#191918] ">
+          Join Community
         </div>
-
       </div>
-        
-    </div>
-  )
-}
 
-export default Movement
+      <div className="w-[1200px] flex items-center justify-center flex-col space-y-10">
+        <div className="w-full h-[443px] rounded-2xl bg-neutral-600"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Movement;
