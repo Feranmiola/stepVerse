89654edae19af95d0ab50543f6ab6c1942a1e2c9
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-30 12:18:45 +0100

    Added movement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Movement from "@/components/Movement";
 import Features from "../components/Features";
 import HowItWorks from "../components/HowItWorks";
 import LeftIcons from "../Icons/LeftIcons";
@@ -5,36 +6,38 @@ import RightIcons from "../Icons/RightIcons";
 import TelegramIcon from "../Icons/TelegramIcon";
 import TwitterIcon from "../Icons/TwitterIcon";
 
-
 export default function Home() {
   return (
     <div className="bg-white w-screen flex flex-col">
-
       <div className="flex flex-row w-full pt-[10rem] items-center justify-between">
-        <LeftIcons/>
+        <LeftIcons />
         <div className="flex flex-col items-center justify-center space-y-5">
           <div className="flex flex-col w-[513.86px] space-y-5">
-            <p  className="text-center text-[60px] font-geist font-medium leading-[3.5rem] text-[#191918]">Get Active, Stay Healthy, and Win Rewards.</p>
-            <p className="text-[17px] w-full text-center text-[#4C4C4C]">StepVerse is the ultimate fitness gaming experience built on Telegram Mini App where we make staying active fun and rewarding.</p>
+            <p className="text-center text-[60px] font-geist font-medium leading-[3.5rem] text-[#191918]">
+              Get Active, Stay Healthy, and Win Rewards.
+            </p>
+            <p className="text-[17px] w-full text-center text-[#4C4C4C]">
+              StepVerse is the ultimate fitness gaming experience built on
+              Telegram Mini App where we make staying active fun and rewarding.
+            </p>
           </div>
           <div className="flex flex-row items-center space-x-5">
             <div className="w-[188.35px] h-[48px] rounded-[41.77px] bg-[#191918] cursor-pointer flex items-center justify-center space-x-3">
-              <TelegramIcon/>
+              <TelegramIcon />
               <p className="text-white text-[16.71px]">Join on Telegram</p>
             </div>
             <div className="w-[188.35px] h-[48px] rounded-[41.77px] bg-[#FBFAF9] cursor-pointer flex items-center justify-center space-x-3">
-              <TwitterIcon/>
+              <TwitterIcon />
               <p className="text-[#080808] text-[16.71px]">Follow on twitter</p>
             </div>
           </div>
-
         </div>
-        <RightIcons/>
+        <RightIcons />
       </div>
 
-      <HowItWorks/>
-      <Features/>
-    
+      <HowItWorks />
+      <Features />
+			<Movement/>
     </div>
   );
 }
---
+++ src/components/Movement.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Movement = () => {
+  return (
+    <div className='w-full pt-20 flex items-center justify-center space-y-5'>
+      <div className='w-[1200px] flex items-center justify-center flex-col space-y-10'>
+
+        <div className='w-[481px] h-[109px] flex flex-col justify-between items-center'>
+          <p className='text-[#191918] text-[32px] font-medium'>Be Part of the Movement</p>
+          <p className='text-center w-full text-[#4C4C4C] text-base'>Join a growing community of users committed to better health. <br/> Together, we can reduce sedentary behavior and build <br/> healthier, happier lives for millions around the globe.</p>
+        </div>
+
+        <div className='w-full h-[443px] rounded-2xl bg-neutral-600'>
+
+        </div>
+
+      </div>
+        
+    </div>
+  )
+}
+
+export default Movement
