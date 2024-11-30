6603d853570b1e3a05c169c982f5a4df294429bc
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-30 10:19:09 +0100

    "Added three identical sections with a rounded rectangle and text to the HowItWorks component, and increased the space between elements from 5 to 10."

--- src/app/components/HowItWorks.tsx
+++ src/app/components/HowItWorks.tsx
@@ -3,10 +3,45 @@ import React from 'react'
 const HowItWorks = () => {
   return (
     <div className='w-full flex pt-20 items-center justify-center'>
-        <div className='w-[1200px] flex flex-col space-y-5'>
+        <div className='w-[1200px] flex flex-col space-y-10'>
             <div className='flex flex-col space-y-2'>
                 <p className='text-[#55C056] text-[15px]'>How it works</p>
                 <p className='text-[32px] w-[341px] font-medium leading-none'>Transform Your Health, One Step at a Time.</p>
+            </div>
+
+
+            <div className='flex w-full justify-between flex-row items-center'>
+
+              <div className='h-[486px] w-full flex flex-col justify-between'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                </div>
+
+                <div className='flex flex-col w-[387px] h-[101px] justify-between'>
+                  <p className='text-[19px] font-semibold text-[#191918]'>Sign Up and Start Moving</p>
+                  <p className='text-[16px] w-full text-[#4C4C4C]'>Join StepVerse through Telegram using your email, WhatsApp, or Telegram account. You’re ready to play in minutes!</p>
+                </div>
+              </div>
+
+              <div className='h-[486px] w-full flex flex-col justify-between'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                </div>
+
+                <div className='flex flex-col w-[387px] h-[101px] justify-between'>
+                  <p className='text-[19px] font-semibold text-[#191918]'>Sign Up and Start Moving</p>
+                  <p className='text-[16px] w-full text-[#4C4C4C]'>Join StepVerse through Telegram using your email, WhatsApp, or Telegram account. You’re ready to play in minutes!</p>
+                </div>
+              </div>
+
+              <div className='h-[486px] w-full flex flex-col justify-between'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                </div>
+
+                <div className='flex flex-col w-[387px] h-[101px] justify-between'>
+                  <p className='text-[19px] font-semibold text-[#191918]'>Sign Up and Start Moving</p>
+                  <p className='text-[16px] w-full text-[#4C4C4C]'>Join StepVerse through Telegram using your email, WhatsApp, or Telegram account. You’re ready to play in minutes!</p>
+                </div>
+              </div>
+
             </div>
 
         </div>
