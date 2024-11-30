f7c25116ac52f9b7a2a2062dbaba094eb7c1a36f
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-30 07:24:26 +0100

    first commit

---
+++ src/app/Icons/TwitterIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TwitterIcon = () => {
+  return (
+    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_91_5545)">
+<path d="M12.3166 9.08015L19.6887 0.510498H17.9415L11.5405 7.95123L6.42785 0.510498H0.530922L8.26226 11.7624L0.530922 20.7488H2.27805L9.03796 12.8911L14.4372 20.7488H20.3341L12.3161 9.08015H12.3166ZM9.9237 11.8613L9.14025 10.7409L2.9075 1.82567H5.59097L10.6207 9.0207L11.4039 10.1411L17.9423 19.4934H15.2592L9.9237 11.8618V11.8613Z" fill="black"/>
+</g>
+<defs>
+<clipPath id="clip0_91_5545">
+<rect width="20.2383" height="20.2383" fill="white" transform="translate(0.312958 0.510498)"/>
+</clipPath>
+</defs>
+</svg>
+
+  )
+}
+
+export default TwitterIcon
