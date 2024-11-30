eded47e3f30ac82a91ff1777c37770ac828fc637
feranmiola <osunjuyigbeiyin@gmail.com>
2024-11-30 17:36:57 +0100

    "Updated HowItWorks component: changed padding, background colors, and text content of various elements."

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HowItWorks = () => {
   return (
-    <div className='w-full flex pt-20 items-center justify-center'>
+    <div className='w-full flex py-20 items-center justify-center'>
         <div className='w-[1200px] flex flex-col space-y-10'>
             <div className='flex flex-col space-y-2'>
                 <p className='text-[#55C056] text-[15px]'>How it works</p>
@@ -13,7 +13,7 @@ const HowItWorks = () => {
             <div className='flex w-full justify-between flex-row items-center'>
 
               <div className='h-[486px] w-full flex flex-col justify-between'>
-                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-[#FBFAF9]'>
                 </div>
 
                 <div className='flex flex-col w-[387px] h-[101px] justify-between'>
@@ -23,22 +23,22 @@ const HowItWorks = () => {
               </div>
 
               <div className='h-[486px] w-full flex flex-col justify-between'>
-                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-[#FBFAF9]'>
                 </div>
 
                 <div className='flex flex-col w-[387px] h-[101px] justify-between'>
-                  <p className='text-[19px] font-semibold text-[#191918]'>Sign Up and Start Moving</p>
-                  <p className='text-[16px] w-full text-[#4C4C4C]'>Join StepVerse through Telegram using your email, WhatsApp, or Telegram account. You’re ready to play in minutes!</p>
+                  <p className='text-[19px] font-semibold text-[#191918]'>Gamify Your Fitness Journey</p>
+                  <p className='text-[16px] w-full text-[#4C4C4C]'>Engage in friendly competitions, treasure hunts, and leaderboard rankings to make fitness a fun, interactive, and rewarding experience.</p>
                 </div>
               </div>
 
               <div className='h-[486px] w-full flex flex-col justify-between'>
-                <div className='w-[387px] h-[360px] rounded-2xl bg-neutral-400'>
+                <div className='w-[387px] h-[360px] rounded-2xl bg-[#FBFAF9]'>
                 </div>
 
                 <div className='flex flex-col w-[387px] h-[101px] justify-between'>
-                  <p className='text-[19px] font-semibold text-[#191918]'>Sign Up and Start Moving</p>
-                  <p className='text-[16px] w-full text-[#4C4C4C]'>Join StepVerse through Telegram using your email, WhatsApp, or Telegram account. You’re ready to play in minutes!</p>
+                  <p className='text-[19px] font-semibold text-[#191918]'>Earn Rewards</p>
+                  <p className='text-[16px] w-full text-[#4C4C4C]'>Turn your steps into SV Points, which can be used to unlock premium features, tools, and other exciting rewards in our marketplace.</p>
                 </div>
               </div>
 
