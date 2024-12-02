5bfd48a2b8853fa40d689717915d29804be5a650
feranmiola <osunjuyigbeiyin@gmail.com>
2024-12-02 11:39:15 +0100

    "Updated font imports and added Inter font from Google Fonts to RootLayout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import { GeistSans } from 'geist/font/sans';
-// import { GeistMono } from 'geist/font/mono';
-
+import { Inter } from 'next/font/google';
 import './globals.css'
+
+const inter = Inter({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="en" className={`${GeistSans.className} ${inter.variable}`}>
       <body>{children}</body>
     </html>
   )
